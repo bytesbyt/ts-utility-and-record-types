b05e47bc63c6fff8d51a7d96ec7d7a29adb6d2ea
bytesbyt <101018180+bytesbyt@users.noreply.github.com>
2025-05-29 18:51:48 +0100

    update with record type

--- detail/main.ts
+++ detail/main.ts
@@ -27,3 +27,48 @@ const updateAddress = (address: Partial<Address>) => {
 }
 
 updateAddress({street:"123"})
+
+// record type
+
+type StringNumberMap = Record<string,number>
+
+const example: StringNumberMap = {
+    apple: 3,
+    orange: 4,
+    mango:4,
+};
+
+type FruitColor = Record <'apple' | 'orange', string>;
+
+const fruitColor: FruitColor = {
+    apple: "red",
+    orange: "ornage",
+};
+
+type UserRole = "admin" | "user" | "guest"
+
+type RolePermission = {
+    admin: string,
+    user: string,
+    guest: string,
+}
+
+type RolePermission2 = Record<UserRole, string>
+
+
+//
+
+type Product = {
+    id: string;
+    name: string;
+    price: number;
+}
+
+type ProductInventory = Record<string, Product>
+
+type ProductIventory = Record<string, Product>
+
+const inventoryResponse: ProductInventory = {
+    apple: {id: "1", name: "Fuji", price: 10,},
+    orange: {id: "2", name: "California", price: 10,}
+}
